d31e31d2c689ce2c227650f0d395c804e5ad1e20
honkling <untitledgeese@gmail.com>
2022-03-20 13:01:12 -0500

    Filter invite links

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { token, ownerId, suggestionsChannel } from '../config.json';
 import { MinehutClient } from './lib/MinehutClient';
 
 const bot = new MinehutClient({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], allowedMentions: { repliedUser: true, roles: [], users: [] } });
+const INVITE_REGEX = /discord(?:\.com|app\.com|\.gg)[\/invite\/]?(?:[a-zA-Z0-9\-]{2,32})/g;
 
 bot.on('ready', () => {
 	console.log(`Logged in as ${bot.user?.tag}.`);
@@ -27,10 +28,33 @@ bot.on('ready', () => {
 	bot.initiateDatabase();
 });
 
-bot.on('messageCreate', (msg: Message) => {
+bot.on('messageCreate', async (msg: Message) => {
 	if(msg.channel.id === suggestionsChannel && !msg.author.bot) {
-		msg.delete();
-		msg.author.send({ content: `Hey, you aren\'t allowed to talk in <#${msg.channel.id}>! If you want to give a suggestion for Minehut+, please use /suggest.`});
+		await msg.delete();
+		await msg.author.send({ content: `Hey, you aren\'t allowed to talk in <#${msg.channel.id}>! If you want to give a suggestion for Minehut+, please use /suggest.`});
+	}
+
+	const invitesRaw = Array.from(msg.content.matchAll(INVITE_REGEX));
+	if(invitesRaw) {
+		// An invite was sent!
+		const invites = invitesRaw[0];
+		for(const link of invites) {
+			const CODE_REGEX = /\/.+$/g;
+			const codeRaw = link.match(CODE_REGEX);
+
+			if(!codeRaw || codeRaw.length === 0)
+				return;
+			
+			// We've got a code! Let's poll the Discord API to view information about the guild so we can filter if needed.
+			const code = codeRaw[0].substring(1);
+			const invite = await bot.fetchInvite(code);
+
+			if(!invite || !invite.guild || invite.guild.id === '872306760394891315')
+				return;
+			
+			// The invite sent wasn't for Minehut+. Let's filter it.
+			await msg.delete();
+		}
 	}
 });
 
