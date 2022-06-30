2b00dbd32ffa2509dc7c9922213b07b50af43123
Awokens <35590883+Awokens@users.noreply.github.com>
2022-06-30 16:19:15 -0700

    Delete src directory

--- src/lib/CommandData.ts
+++
@@ -1,5 +0,0 @@
-import { ApplicationCommandData, ApplicationCommandPermissionData } from "discord.js";
-
-export type CommandData = ApplicationCommandData & {
-    permissions?: ApplicationCommandPermissionData[];
-};
